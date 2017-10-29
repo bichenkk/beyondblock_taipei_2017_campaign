dc3c8a8f743643f7c73c066def101423999bd0ca
Ben Lu <coopermilk123@gmail.com>
2017-10-30 01:15:26 +0800

    Load fonts before rendering.

--- src/App.vue
+++ src/App.vue
@@ -77,7 +77,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import url(https://fonts.googleapis.com/css?family=Open+Sans|Lato);
 $font-family: 'Open+Sans', 'Lato', sans-serif, Helvetica, Arial;
 
 .navbar-cover {
