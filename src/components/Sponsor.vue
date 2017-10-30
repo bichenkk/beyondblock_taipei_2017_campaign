628fae66d560333261560d73929d440a4c2d88c7
Ben Lu <coopermilk123@gmail.com>
2017-10-30 23:28:05 +0800

    Implement sponsor component.

--- src/components/Sponsor.vue
+++ src/components/Sponsor.vue
@@ -1,10 +1,46 @@
 <template>
-  <div class="sponsor">
+  <section class="sponsor">
     <div class="title">
       <i class="title__icon"></i>
       <h1 class="title__wording">贊助商</h1>
     </div>
-  </div>
+
+    <div class="sponsor__rank">
+      <h3>Platinum Sponsors</h3>
+    </div>
+
+    <div class="sponsor__box">
+      <div class="sponsor__row">
+        <div class="sponsor__logo"></div>
+        <div class="sponsor__logo"></div>
+        <div class="sponsor__logo"></div>
+      </div>
+    </div>
+
+    <div class="sponsor__rank">
+      <h3>Gold Sponsors</h3>
+    </div>
+
+    <div class="sponsor__box">
+      <div class="sponsor__row">
+        <div class="sponsor__logo"></div>
+        <div class="sponsor__logo"></div>
+        <div class="sponsor__logo"></div>
+      </div>
+    </div>
+
+    <div class="sponsor__rank">
+      <h3>Silver Sponsors</h3>
+    </div>
+
+    <div class="sponsor__box">
+      <div class="sponsor__row">
+        <div class="sponsor__logo"></div>
+        <div class="sponsor__logo"></div>
+        <div class="sponsor__logo"></div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -13,5 +49,71 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+$font-family: 'Open+Sans', 'Lato', sans-serif, Helvetica, Arial;
+
+.sponsor {
+  &__rank {
+    text-align: center;
+  }
+  &__rank h3 {
+    font-family: $font-family;
+    font-size: 24px;
+    color: #4A4A4A;
+    letter-spacing: 0;
+  }
+  &__row {
+    width: 800px;
+    margin: 0 auto;
+    margin-bottom: 40px;
+  }
+  &__logo {
+    margin: 15px 30px;
+    display: inline-block;
+    width: 200px;
+    height: 135px;
+    border: 1px solid #E1DFE2;
+    border-radius: 10px;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .sponsor {
+    &__row {
+      width: 610px;
+      margin: 0 auto;
+      margin-bottom: 40px;
+    }
+    &__logo {
+      display: inline-block;
+      margin: 0 auto;
+      margin-top: 30px;
+      margin-bottom: 30px;
+      width: 200px;
+      height: 135px;
+      border: 1px solid #E1DFE2;
+      border-radius: 10px;
+    }
+  }
+}
+
+@media only screen and (max-width: 575px) {
+  .sponsor {
+    &__row {
+      width: 100%;
+      margin: 0 auto;
+      margin-bottom: 40px;
+    }
+    &__logo {
+      display: block;
+      margin: 0 auto;
+      margin-top: 30px;
+      margin-bottom: 30px;
+      width: 266px;
+      height: 135px;
+      border: 1px solid #E1DFE2;
+      border-radius: 10px;
+    }
+  }
+}
 </style>
