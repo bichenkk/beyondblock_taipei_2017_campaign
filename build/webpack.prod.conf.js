45c70dd9272379092fe55b7390365907c926a7aa
Juin Chiu <davidjuin0519@gmail.com>
2017-11-06 21:03:51 +0800

    (fix): Config webpack to fit github page

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -22,6 +22,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
+    publicPath: "/bbt2017/",
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
